0a1a0d3a18f4b4afbb65ad4ddbf84a0494fd469d
yurii.nikitin <y.nikitin@wpt.dev>
2022-07-11 09:36:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# WPT-FS-COURSE
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+```
+node index.js --action list
+```
+![alt text](https://i.ibb.co/QDJ4nfK/2022-07-11-09-27.png)
+
+# Получаем контакт по id
+```
+node index.js --action get --id 5
+```
+![alt text](https://i.ibb.co/q0k6JWw/action-get.png)
+
+# Добавялем контакт
+```
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+```
+![alt text](https://i.ibb.co/Dw3dnyh/action-add.png)
+
+# Удаляем контакт
+```
+node index.js --action remove --id=3
+```
+![alt text](https://i.ibb.co/ZTKCcRf/remove.png)
+
---
+++ contacts.js
@@ -0,0 +1,62 @@
+import fs from "fs/promises";
+
+const contactsPath = `./db/contacts.json`;
+
+// Return contacts list from DB file
+async function listContacts() {
+    try {
+        const contactList = await fs.readFile(contactsPath);
+        return JSON.parse(contactList);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Get signle contact by ID
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+
+    return contacts.find(item => item.id === contactId) ?? `Contact with id ${contactId} not found`;
+}
+
+// Remove single contact by ID
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+
+    const newContacts = contacts.filter(item => {
+        return item.id !== contactId;
+    });
+
+    await updateContacts(newContacts);
+    return newContacts;
+}
+
+// Add bew contact to DB file
+async function addContact(name, email, phone) {
+    if (!name || !email || !phone) {
+        return 'Please enter correct params (Name, email, phone)'
+    }
+
+    const contacts = await listContacts();
+    const contact = {
+        id: (contacts.length + 1).toString(),
+        name,
+        email,
+        phone
+    };
+
+    contacts.push(contact);
+    await updateContacts(contacts);
+    return contact;
+}
+
+// Update contacts in DB file
+const updateContacts = async (contacts) => {
+    try {
+        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default { listContacts, getContactById, removeContact, addContact };
